2d58cec57b4f3f4b299ceb03cf99da41f136d16a
ilkinsufi <ilkinsufi@gmail.com>
2024-06-17 22:36:53 +0400

    drawer added

--- App.js
+++ App.js
@@ -2,18 +2,37 @@ import React from "react";
 import { Platform, StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MetalDetailScreen from "./screens/MetalDetailScreen";
+import FavoritesScreen from "./screens/FavoritesScreen";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
+  function DrawerNavigator() {
+    return (
+      <Drawer.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: "#3f2f25" },
+          headerTintColor: "white",
+          headerTitleAlign: "center",
+          contentStyle: { backgroundColor: "#3f2f25" },
+        }}
+      >
+        <Drawer.Screen name="Categories" component={CategoriesScreen} />
+        <Drawer.Screen name="Favotites" component={FavoritesScreen} />
+      </Drawer.Navigator>
+    );
+  }
+
   return (
     <>
       <StatusBar
-        barStyle={Platform.OS === "ios" ? "light-content" : "light-content"}
+        barStyle={Platform.OS === "ios" ? "light-content" : "default"}
       />
       <NavigationContainer>
         <Stack.Navigator
@@ -25,9 +44,9 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name="MealsCategories"
-            component={CategoriesScreen}
-            options={{ title: "All Cagegories" }}
+            name="Drawer"
+            component={DrawerNavigator}
+            options={{ headerShown: false }}
           />
           <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
           <Stack.Screen name="MealDetail" component={MetalDetailScreen} />
